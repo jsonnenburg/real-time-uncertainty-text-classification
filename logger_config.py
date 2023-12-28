37df147ad7dc794b1b52debf8c0c499263bdf102
Johann Sonnenburg <johann.sonnenburg@data-spree.com>
2023-12-28 12:38:54 +0100

    adapt transfer loss functions to BERT output format, make logger output to file optional

--- logger_config.py
+++ logger_config.py
@@ -1,16 +1,18 @@
 import logging
+from typing import Optional
 
 
-def setup_logging(log_file_path):
+def setup_logging(log_file_path: Optional[str] = None):
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
     c_handler = logging.StreamHandler()
-    f_handler = logging.FileHandler(log_file_path)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     c_handler.setFormatter(formatter)
-    f_handler.setFormatter(formatter)
-
     logger.addHandler(c_handler)
-    logger.addHandler(f_handler)
+
+    if log_file_path is not None:
+        f_handler = logging.FileHandler(log_file_path)
+        f_handler.setFormatter(formatter)
+        logger.addHandler(f_handler)
--- src/utils/loss_functions.py
+++ src/utils/loss_functions.py
@@ -1,11 +1,18 @@
 import tensorflow as tf
 
+from logger_config import setup_logging
+import logging
 
-def null_loss(y_true, y_pred):
+logger = logging.getLogger(__name__)
+
+setup_logging()
+
+
+def null_loss(y_true, y_pred) -> tf.Tensor:
     return tf.zeros_like(y_true)
 
 
-def aleatoric_loss(y_true, y_pred):
+def aleatoric_loss(y_true, y_pred) -> tf.Tensor:
     """
     Aleatoric uncertainty loss function from Kendall & Gal (2017) for fine-tuning the teacher model.
     Does not require ground truth variance.
@@ -33,38 +40,43 @@ def aleatoric_loss(y_true, y_pred):
     return tf.reduce_mean(adjusted_loss)
 
 
-def gaussian_mle_loss(y_true, y_pred_mean, y_pred_log_variance):
+def gaussian_mle_loss(y_true, y_pred) -> tf.Tensor:
     """
     Gaussian MLE loss function from Shen et al. (2021) for fine-tuning the student model on the teacher's predictions.
     """
-    mu_i, log_variance_i = y_pred_mean, y_pred_log_variance
+    try:
+        logits, log_variances = y_pred['logits'], y_pred['log_variances']
+        # calculate the Gaussian MLE loss
+        # loss = 0.5 * tf.reduce_mean(variance_i + tf.square(y_true - mu_i) / variance_i - log_variance_i)
+        loss = tf.reduce_mean(0.5 * tf.exp(-log_variances) * tf.square(y_true - logits) + 0.5 * log_variances)
+        # TODO: check if this corresponds to Eq. 10 in Shen et al. (2021)
+        return loss
+    except KeyError:
+        logger.error("y_pred must be a dict with keys 'logits' and 'log_variances'.")
+        raise
 
-    # calculate the Gaussian MLE loss
-    # loss = 0.5 * tf.reduce_mean(variance_i + tf.square(y_true - mu_i) / variance_i - log_variance_i)
-    loss = tf.reduce_mean(0.5 * tf.exp(-log_variance_i) * tf.square(y_true - mu_i) + 0.5 * log_variance_i)
-    # TODO: check if this corresponds to Eq. 10 in Shen et al. (2021)
-    return loss
 
-
-def shen_loss(y_true, y_pred, weight=1):
+def shen_loss(y_true, y_pred) -> tf.Tensor:
     """
     Transfer learning loss function from Shen et al. (2021) for fine-tuning the student model.
     Weight corresponds to Lambda in the paper.
 
     y_true = Tuple(actual ground truth, teacher predictive sample)
     """
-    # TODO: update to use y_pred dict and and proper tf-compliant losses (see aleatoric loss)
+    weight = tf.convert_to_tensor(1, dtype=tf.float32)
 
-    assert y_true.shape[1] == 2 and y_pred.shape[1] == 2
+    try:
+        y_true, y_teacher = y_true['labels'], y_true['predictions']
 
-    y_true, y_teacher_pred_mean = y_true[:, 0], y_true[:, 1]
-    # unpack the predictions, this assumes that the predictions are a vector of size 2
-    y_student_pred_mean, y_student_pred_log_variance = y_pred[:, 0], y_pred[:, 1]
-    Lt = aleatoric_loss(y_true, y_pred)
-    Ls = gaussian_mle_loss(y_teacher_pred_mean, y_student_pred_mean, y_student_pred_log_variance)
-    Ltotal = Ls + weight * Lt
+        Lt = aleatoric_loss(y_true, y_pred)
+        Ls = gaussian_mle_loss(y_teacher, y_pred)
+        Ltotal = Ls + weight * Lt
 
-    return tf.reduce_mean(Ltotal)
+        return tf.reduce_mean(Ltotal)
+    except KeyError:
+        logger.error("y_true must be a dict with keys 'labels' and 'predictions'.")
+        raise
+
 
 # shen loss function - implementation for classification in pytorch
 # def shen_loss(y_true, y_pred):
