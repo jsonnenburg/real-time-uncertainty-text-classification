fix - compute std dev from log variance

distribution_distillation/sample_from_teacher.py
@@ -116,7 +116,7 @@ def aleatoric_mc_dropout_transfer_sampling(model, data: tf.data.Dataset, m: int 
         mu_t = tf.nn.sigmoid(all_logits)  # shape is (m, batch_size, num_classes)
 
         # sigma_hat_sq = tf.math.reduce_variance(all_logits, axis=0)
-        sigma_hat = all_log_variances  # tf.math.sqrt(sigma_hat_sq)  # TODO: replace with log_variances from output!
+        sigma_hat = tf.sqrt(tf.exp(all_log_variances))
         sigma_tilde = tf.reduce_mean(sigma_hat, axis=1)
         sigma_tilde_reshaped = tf.reshape(sigma_tilde, [1, -1, 1])  # reshape to (1, batch_size, 1)
 