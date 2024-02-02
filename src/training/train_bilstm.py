0d865f661a75b7e1b6e50d65350704eedd3ec424
Johann Sonnenburg <johann.sonnenburg@data-spree.com>
2024-02-02 16:45:51 +0100

    add progress on bilstm training

--- src/training/train_bilstm.py
+++ src/training/train_bilstm.py
@@ -1,13 +1,15 @@
 import argparse
 import json
 import os
+import time
 
+import numpy as np
 import tensorflow as tf
 import logging
 
 from sklearn.metrics import classification_report
 
-from src.utils.training import HistorySaver
+from src.utils.training import HistorySaver, BiLSTMConfig
 
 logger = logging.getLogger(__name__)
 
@@ -15,18 +17,116 @@ from src.data.robustness_study.baselines_data_preprocessing import pad_sequences
 from src.utils.data import SimpleDataLoader
 from src.utils.logger_config import setup_logging
 
+from src.utils.metrics import (serialize_metric, accuracy_score, precision_score, recall_score, f1_score, nll_score,
+                               brier_score, ece_score, bald_score)
+
 from src.models.bilstm_model import create_bilstm_config, BiLSTM
 
 
-def compute_metrics(model, eval_data):
-    pass
+def compute_metrics(model, eval_data) -> dict:
+    total_logits = []
+    total_labels = []
+
+    # iterate over all batches in eval_data
+    start_time = time.time()
+    for batch in eval_data:
+        features, labels = batch
+        logits = model(features, training=False)
+        total_logits.extend(logits)
+        total_labels.extend(labels.numpy())
+    total_time = time.time() - start_time
+    average_inference_time = total_time / len(total_labels) * 1000
+    logger.info(f"Average inference time per sample: {average_inference_time:.0f} milliseconds.")
+
+    all_labels = np.array(total_labels)
+
+    y_prob = tf.nn.sigmoid(total_logits).numpy().reshape(all_labels.shape)
+    y_pred = y_prob.round(0).astype(int)
+    y_true = all_labels
+
+    acc = accuracy_score(y_true, y_pred)
+    prec = precision_score(y_true, y_pred)
+    rec = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    nll = nll_score(y_true, y_prob)
+    bs = brier_score(y_true, y_prob)
+    ece = ece_score(y_true, y_pred, y_prob)
+    return {
+        "y_true": y_true.tolist(),
+        "y_pred": y_pred.tolist(),
+        "y_prob": y_prob.tolist(),
+        "average_inference_time": serialize_metric(average_inference_time),
+        "accuracy_score": serialize_metric(acc),
+        "precision_score": serialize_metric(prec),
+        "recall_score": serialize_metric(rec),
+        "f1_score": serialize_metric(f1),
+        "nll_score": serialize_metric(nll),
+        "brier_score": serialize_metric(bs),
+        "ece_score": serialize_metric(ece)
+    }
 
 
-def compute_mc_dropout_metrics(model, eval_data):
-    pass
+def compute_mc_dropout_metrics(model, eval_data, n=50) -> dict:
+    total_logits = []
+    total_probs = []
+    total_mean_logits = []
+    total_var_logits = []
+    total_labels = []
+
+    start_time = time.time()
+    for batch in eval_data:
+        features, labels = batch
+
+        samples = model.mc_dropout_sample(features, n=n)
+        logits = samples['logit_samples']
+        probs = samples['prob_samples']
+        mean_logits = samples['mean_logits']
+        var_logits = samples['var_logits']
+        total_logits.append(logits.numpy())
+        total_probs.append(probs.numpy())
+        total_mean_logits.extend(mean_logits.numpy())
+        total_var_logits.extend(var_logits.numpy())
+        total_labels.extend(labels.numpy())
+    total_time = time.time() - start_time
+    average_inference_time = total_time / len(total_labels) * 1000
+    logger.info(f"Average inference time per sample: {average_inference_time:.0f} milliseconds.")
+
+    y_prob_samples = np.concatenate(total_probs, axis=0)
+
+    all_labels = np.array(total_labels)
+
+    y_prob_mcd = tf.nn.sigmoid(total_mean_logits).numpy().reshape(all_labels.shape)
+    y_pred_mcd = y_prob_mcd.round(0).astype(int)
+    y_true = all_labels
+
+    var_logits = np.array(total_var_logits).reshape(all_labels.shape)
+
+    acc = accuracy_score(y_true, y_pred_mcd)
+    prec = precision_score(y_true, y_pred_mcd)
+    rec = recall_score(y_true, y_pred_mcd)
+    f1 = f1_score(y_true, y_pred_mcd)
+    nll = nll_score(y_true, y_prob_mcd)
+    bs = brier_score(y_true, y_prob_mcd)
+    ece = ece_score(y_true, y_pred_mcd, y_prob_mcd)
+    bald = bald_score(y_prob_samples)
+    return {
+        "y_true": y_true.tolist(),
+        "y_pred": y_pred_mcd.tolist(),
+        "y_prob": y_prob_mcd.tolist(),
+        "var_logits": var_logits.tolist(),
+        "average_inference_time": serialize_metric(average_inference_time),
+        "accuracy_score": serialize_metric(acc),
+        "precision_score": serialize_metric(prec),
+        "recall_score": serialize_metric(rec),
+        "f1_score": serialize_metric(f1),
+        "nll_score": serialize_metric(nll),
+        "brier_score": serialize_metric(bs),
+        "ece_score": serialize_metric(ece),
+        "bald_score": bald.tolist()
+    }
 
 
-def main(args):
+def prepare_data(args):
     data_loader = SimpleDataLoader(dataset_dir=args.input_data_dir)
     try:
         data_loader.load_dataset()
@@ -54,72 +154,162 @@ def main(args):
 
     word_index = tokenizer.word_index
 
-    glove_file = 'data/glove.6B.100d.txt'
+    glove_file = 'data/glove.6B.200d.txt'
     glove_embeddings = load_glove_embeddings(glove_file)
-    embedding_matrix = get_embedding_matrix(word_index, glove_embeddings, args.glove_vector_size)
+    embedding_matrix = get_embedding_matrix(word_index, glove_embeddings, 200)
 
     y_train = dataset.train['target'].values
+    y_val = dataset.val['target'].values
+    y_test = dataset.test['target'].values
 
-    config = create_bilstm_config(embedding_dropout_rate=args.embedding_dropout_rate,
-                                    hidden_dropout_rate=args.hidden_dropout_rate,
-                                    lstm_units_1=args.lstm_units_1,
-                                     lstm_units_2=args.lstm_units_2)
+    data = {'X_train': X_train,
+            'X_val': X_val,
+            'X_test': X_test,
+            'y_train': y_train,
+            'y_val': y_val,
+            'y_test': y_test,
+            'embedding_matrix': embedding_matrix,
+            'max_length': max_length}
 
-    model = BiLSTM(config, embedding_matrix=embedding_matrix, sequence_length=max_length)
+    return data, embedding_matrix, max_length
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
-    model.compile(optimizer=optimizer,
-                  loss='binary_crossentropy',
-                  metrics=[tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.Precision(), tf.keras.metrics.Recall()],
-                  run_eagerly=True)
+
+def train_bilstm(args, data: dict, config: BiLSTMConfig, save_model: bool = False):
+    model = BiLSTM(config, embedding_matrix=data['embedding_matrix'], sequence_length=data['max_length'])
 
     config_info = {
-        'embedding_dropout_rate': args.embedding_dropout_rate,
-        'hidden_dropout_rate': args.hidden_dropout_rate,
-        'lstm_units_1': args.lstm_units_1,
-        'lstm_units_2': args.lstm_units_2,
-        'max_vocab_size': args.max_vocab_size,
-        'glove_vector_size': args.glove_vector_size,
-        'max_length': args.max_length,
+        'embedding_dropout_rate': config.embedding_dropout_rate,
+        'hidden_dropout_rate': config.hidden_dropout_rate,
+        'lstm_units_1': config.lstm_units_1,
+        'lstm_units_2': config.lstm_units_2,
+        'max_length': data['max_length'],
         'learning_rate': args.learning_rate,
         'batch_size': args.batch_size,
         'epochs': args.epochs
     }
-    model_dir = os.path.join(args.output_dir, 'model')
-    os.makedirs(model_dir, exist_ok=True)
-    with open(os.path.join(model_dir, 'config.json'), 'w') as f:
-        json.dump(config_info, f)
 
-    checkpoint_path = os.path.join(model_dir, 'cp-{epoch:02d}.ckpt')
+    if save_model:
+        model_dir = os.path.join(args.output_dir, 'model')
+        os.makedirs(model_dir, exist_ok=True)
+        with open(os.path.join(model_dir, 'config.json'), 'w') as f:
+            json.dump(config_info, f)
 
-    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                     save_weights_only=True,
-                                                     verbose=1)
+        checkpoint_path = os.path.join(model_dir, 'cp-{epoch:02d}.ckpt')
 
-    history_callback = HistorySaver(file_path=os.path.join(log_dir, 'bilstm_train.txt'))
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                         save_weights_only=True,
+                                                         verbose=1)
 
-    latest_checkpoint = tf.train.latest_checkpoint(model_dir)
-    if latest_checkpoint:
-        model.load_weights(latest_checkpoint).expect_partial()
-        logger.info(f"Found model files, loaded weights from {latest_checkpoint}")
-        logger.info('Skipping training.')
+        history_callback = HistorySaver(file_path=os.path.join(log_dir, 'bilstm_train.txt'))
+
+        optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
+        model.compile(optimizer=optimizer,
+                      loss='binary_crossentropy',
+                      metrics=[tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.Precision(), tf.keras.metrics.Recall()],
+                      run_eagerly=True)
+
+        latest_checkpoint = tf.train.latest_checkpoint(model_dir)
+        if latest_checkpoint:
+            model.load_weights(latest_checkpoint).expect_partial()
+            logger.info(f"Found model files, loaded weights from {latest_checkpoint}")
+            logger.info('Skipping training.')
+        else:
+            model.fit(data['X_train'], data['y_train'],
+                      batch_size=args.batch_size,
+                      epochs=args.epochs,
+                      validation_data=(data['X_val'], data['y_val']),
+                      callbacks=[cp_callback, history_callback])
+
+        result_dir = os.path.join(args.output_dir, 'results')
+        os.makedirs(result_dir, exist_ok=True)
+
+        eval_metrics = compute_metrics(model, data['X_val'])
+        with open(os.path.join(result_dir, 'results_stochastic_pass.json'), 'w') as f:
+            json.dump(eval_metrics, f)
+        logger.info(
+            f"\n==== Classification report  (weight averaging) ====\n {classification_report(eval_metrics['y_true'], eval_metrics['y_pred'], zero_division=0)}")
+
+        logger.info("Computing MC dropout metrics.")
+        mc_dropout_metrics = compute_mc_dropout_metrics(model, data['X_val'])
+        with open(os.path.join(result_dir, 'results.json'), 'w') as f:
+            json.dump(mc_dropout_metrics, f)
+        logger.info(f"\n==== Classification report  (MC dropout) ====\n {classification_report(mc_dropout_metrics['y_true'], mc_dropout_metrics['y_pred'], zero_division=0)}")
     else:
-        model.fit(X_train, y_train, batch_size=args.batch_size, epochs=args.epochs, validation_data=(X_val, dataset.val['target'].values))
+        optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
+        model.compile(optimizer=optimizer,
+                      loss='binary_crossentropy',
+                      metrics=[tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.Precision(),
+                               tf.keras.metrics.Recall()],
+                      run_eagerly=True)
+        model.fit(data['X_train'], data['y_train'],
+                  batch_size=args.batch_size,
+                  epochs=args.epochs,
+                  validation_data=(data['X_val'], data['y_val']))
+        eval_metrics = compute_metrics(model, data['X_val'])
+        logger.info(
+            f"\n==== Classification report  (weight averaging) ====\n {classification_report(eval_metrics['y_true'], eval_metrics['y_pred'], zero_division=0)}")
 
-    eval_data = val_data if val_data is not None else test_data
-    eval_metrics = compute_metrics(model, eval_data)
-    with open(os.path.join(paths['results_dir'], 'results_stochastic_pass.json'), 'w') as f:
-        json.dump(eval_metrics, f)
-    logger.info(
-        f"\n==== Classification report  (weight averaging) ====\n {classification_report(eval_metrics['y_true'], eval_metrics['y_pred'], zero_division=0)}")
+        logger.info("Computing MC dropout metrics.")
+        mc_dropout_metrics = compute_mc_dropout_metrics(model, data['X_val'])
+        logger.info(
+            f"\n==== Classification report  (MC dropout) ====\n {classification_report(mc_dropout_metrics['y_true'], mc_dropout_metrics['y_pred'], zero_division=0)}")
 
-    logger.info("Computing MC dropout metrics.")
-    mc_dropout_metrics = compute_mc_dropout_metrics(model, eval_data)
-    with open(os.path.join(paths['results_dir'], 'results.json'), 'w') as f:
-        json.dump(mc_dropout_metrics, f)
-    logger.info(
-        f"\n==== Classification report  (MC dropout) ====\n {classification_report(mc_dropout_metrics['y_true'], mc_dropout_metrics['y_pred'], zero_division=0)}")
     return mc_dropout_metrics
+
+
+def run_bilstm_grid_search(args, data: dict, embedding_dropout_rates: list, hidden_dropout_rates: list, lstm_units_1: list, lstm_units_2: list):
+    best_config = None
+    best_f1 = 0
+    for edr in embedding_dropout_rates:
+        for hdr in hidden_dropout_rates:
+            for lu1 in lstm_units_1:
+                for lu2 in lstm_units_2:
+                    config = create_bilstm_config(embedding_dropout_rate=edr,
+                                                  hidden_dropout_rate=hdr,
+                                                  lstm_units_1=lu1,
+                                                  lstm_units_2=lu2)
+                    try:
+                        logger.info(f"Training BiLSTM with config: {config}")
+                        result = train_bilstm(args, data, config, save_model=False)
+                        f1 = result['f1']
+                        if f1 > best_f1:
+                            best_f1 = f1
+                            best_config = config
+                            logger.info(f"New best config: {config} with F1: {f1}")
+                    except Exception as e:
+                        logger.error(f"Failed to train BiLSTM with config: {config} due to: {e}")
+    logger.info("Finished grid search.")
+    logger.info(f"Best config: {best_config} with F1: {best_f1}")
+    return best_f1, best_config
+
+
+def main(args):
+    # load and preprocess data
+    data, embedding_matrix, max_length = prepare_data(args)
+
+    # grid search for best hyperparameters
+    embedding_dropout_rates = [0.1, 0.2, 0.3, 0.4, 0.5]
+    hidden_dropout_rates = [0.1, 0.2, 0.3, 0.4, 0.5]
+    lstm_units_1 = [32, 64, 128, 256]
+    lstm_units_2 = [16, 32, 64, 128]
+    best_f1, best_config = run_bilstm_grid_search(args, data, embedding_dropout_rates, hidden_dropout_rates, lstm_units_1, lstm_units_2)
+
+    # train BiLSTM with best hyperparameters
+    # prepare combined training and validation data
+    data['X_train'] = tf.concat([data['X_train'], data['X_val']], axis=0)
+    data['y_train'] = tf.concat([data['y_train'], data['y_val']], axis=0)
+    data['X_val'] = data['X_test']
+    data['y_val'] = data['y_test']
+
+    best_config = BiLSTMConfig(embedding_dropout_rate=best_config.embedding_dropout_rate,
+                                 hidden_dropout_rate=best_config.hidden_dropout_rate,
+                                    lstm_units_1=best_config.lstm_units_1,
+                                    lstm_units_2=best_config.lstm_units_2)
+
+    results = train_bilstm(args, data, best_config, save_model=True)
+
+    logger.info(f"Final F1: {results['f1']}")
+    logger.info("Finished training.")
 
 
 if __name__ == "__main__":
