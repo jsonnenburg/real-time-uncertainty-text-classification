a248202e5431cbad5a267fd90085de1b4956ae46
Johann Sonnenburg <johann.sonnenburg@data-spree.com>
2024-02-19 17:46:57 +0100

    add script for performing grid search for augmented transfer set

---
+++ src/distribution_distillation/run_augmented_student_gridsearch.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --job-name=rtuq-augment-student-gridsearch
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:a10080gb:3
+#SBATCH --mem=32G
+#SBATCH --output=shen-student-augmentation-gridsearch_%j.out
+#SBATCH --time=30:00:00
+
+module load python/3.8
+module load cuda/11.3
+
+export PYTHONPATH="/vol/fob-vol1/nebenf23/sonnenbj/real-time-uncertainty-text-classification/"
+
+python3.8 -m venv env
+source env/bin/activate
+echo "PYTHONPATH after activating venv: $PYTHONPATH"
+pip install --upgrade pip
+pip install -r slurm_requirements.txt
+
+shen_loss_weights=(0.5 1 2)
+learning_rates=(0.0002 0.00002 0.000002)
+epochs_list=(2 3 4)
+
+for shen_loss_weight in "${shen_loss_weights[@]}"; do
+    for learning_rate in "${learning_rates[@]}"; do
+        for epochs in "${epochs_list[@]}"; do
+            version_identifier="shen_${shen_loss_weight}_lr${learning_rate}_e${epochs}_augmented"
+            python3.8 src/distribution_distillation/train_augmented_student.py \
+            --transfer_data_dir data/distribution_distillation \
+            --teacher_model_save_dir out/bert_teacher/final_e3_lr2_hd020_ad020_cd030/model \
+            --version_identifier "$version_identifier" \
+            --shen_loss_weight $shen_loss_weight \
+            --learning_rate $learning_rate \
+            --batch_size 32 \
+            --epochs $epochs \
+            --max_length 48 \
+            --m 5 \
+            --k 10 \
+            --output_dir out/bert_student_augmented \
+            --remove_dropout_layers
+        done
+    done
+done
